0173c2ce1701093af5a5a3e18466c709db593f6d
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-04-12 17:42:56 -0400

    Output buffering

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,7 @@
 <?php
+    // Output buffering
+    ob_start();
+
     define("PRIVATE_PATH", dirname(__FILE__));
     define("PROJECT_PATH", dirname(PRIVATE_PATH));
     define("PUBLIC_PATH", PROJECT_PATH . "./public");
