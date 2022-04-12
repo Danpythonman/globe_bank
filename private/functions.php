e3f5bc265e0cb966454deae732b19740a516de0e
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-04-12 17:35:59 -0400

    Added 404 and 500 errors and 302 redirect

--- private/functions.php
+++ private/functions.php
@@ -8,4 +8,19 @@
         return WWW_ROOT . $script_path;
     }
 
+    function error_404() {
+        header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found');
+        exit();
+    }
+
+    function error_500() {
+        header($_SERVER['SERVER_PROTOCOL'] . '500 Internal Server Error');
+        exit();
+    }
+
+    function redirect_to($location) {
+        header('Location: ' . $location);
+        exit;
+    }
+
 ?>
