74cc5f86e0b290008004d2fcac1efdfd7cd99861
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-06-05 15:16:20 -0400

    Added information (from database) to show subject page.

--- private/query_functions.php
+++ private/query_functions.php
@@ -13,6 +13,22 @@
         return $subject_set;
     }
 
+    function find_subject_by_id($id) {
+        global $db;
+
+        $query = "SELECT * FROM subjects WHERE id='$id'";
+      
+        $subject_set = mysqli_query($db, $query);
+      
+        confirm_result_set($subject_set);
+      
+        $subject = mysqli_fetch_assoc($subject_set);
+      
+        mysqli_free_result($subject_set);
+
+        return $subject;
+    }
+
     function find_all_pages() {
         global $db;
 
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,6 +1,10 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php $id = htmlspecialchars($_GET['id'] ?? "1"); ?>
+<?php
+  $id = htmlspecialchars($_GET['id'] ?? "1");
+
+  $subject = find_subject_by_id($id);
+?>
 
 <?php $page_title = 'Show Subject' ?>
 
@@ -9,7 +13,21 @@
 <div id="content">
   <a class="back-link" href="<?php echo url_for('staff/subjects/index.php'); ?>">&laquo; Back to Subject List</a>
   <div class="subject show">
-    Page ID: <?php echo htmlspecialchars($id); ?>
+    <h1>Subject: <?php echo htmlspecialchars($subject['menu_name']); ?></h1>
+    <div class="attributes">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><?php echo htmlspecialchars($subject['menu_name']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd><?php echo htmlspecialchars($subject['position']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+      </dl>
+    </div>
   </div>
 </div>
 
