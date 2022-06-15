b07ec070f79e25925131aae6f39f75af78cb9925
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-06-14 21:52:27 -0400

    Added functionality (with php and javascript) to have the positions in the dropdown reflect the number of pages in the selected subject when editing a new page.

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -16,7 +16,7 @@
 
     $page = [
       'id' => $id,
-      'subject_id' => 1,
+      'subject_id' => $_POST['subject_id'] ?? '',
       'menu_name' => $_POST['menu_name'] ?? '',
       'position' => $_POST['position'] ?? '',
       'visible' => $_POST['visible'] ?? '',
@@ -29,6 +29,10 @@
 
   } else {
     $page = find_page_by_id($id);
+
+    $subject_names_and_ids = get_subject_names_and_ids();
+
+    $position_list = count_pages_in_subjects();
   }
 
 ?>
@@ -49,10 +53,28 @@
         </dd>
       </dl>
       <dl>
+        <dt>Subject</dt>
+        <dd>
+          <select name="subject_id" id="subject-select">
+            <?php
+              foreach ($subject_names_and_ids as $subject_name_and_id) {
+                echo "<option value=\"{$subject_name_and_id['id']}\"";
+
+                if ($subject_name_and_id['id'] == $page['subject_id']) {
+                  echo " selected";
+                }
+
+                echo ">{$subject_name_and_id['name']}</option>";
+              }
+            ?>
+          </select>
+        </dd>
+      </dl>
+      <dl>
         <dt>Position</dt>
         <dd>
-          <select name="position">
-            <option value="1" <?php echo $page['position'] == '1' ? 'selected' : '' ?>>1</option>
+          <select name="position" id="position-select">
+            <!-- Will be filled in with Javascript -->
           </select>
         </dd>
       </dl>
@@ -69,5 +91,47 @@
     </form>
   </div>
 </div>
+
+<script>
+  let currentSubjectId;
+
+  <?php echo "currentSubjectId = {$page['subject_id']};" ?>
+
+  let countsOfSubjectsPages = {};
+
+  <?php foreach ($subject_names_and_ids as $subject_name_and_id) {
+    $subject_id = $subject_name_and_id['id'];
+
+    $count_for_subject = $position_list[$subject_id] ?? 0;
+
+    echo "countsOfSubjectsPages[$subject_id] = $count_for_subject;"
+  ?>
+  <?php } ?>
+
+  document.getElementById('subject-select').onchange = (event) => updatePositionDropdown(event.target);
+
+  function updatePositionDropdown(subjectDropdown) {
+    const subjectId = subjectDropdown.value;
+
+    const subjectMaxPosition = currentSubjectId == subjectDropdown.value
+      ? countsOfSubjectsPages[subjectId]
+      : countsOfSubjectsPages[subjectId] + 1;
+
+    const positionSelect = document.getElementById('position-select');
+
+    positionSelect.innerHTML = "";
+
+    for (let i = 1; i <= subjectMaxPosition; i++) {
+      let option = document.createElement('option');
+
+      option.value = i;
+      option.innerHTML = i;
+
+      positionSelect.appendChild(option);
+    }
+  }
+
+  updatePositionDropdown(document.getElementById('subject-select'));
+</script>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
